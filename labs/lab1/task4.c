9f6bc24f3cf230b4dffcd1c99dfb938f9d952a01
danx-05 <danekdenek203@gmail.com>
2024-09-03 11:05:02 +0500

    Create task4.c

---
+++ labs/lab1/task4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    
+    float a;
+    float b;
+    float c;
+    
+    
+    printf("Input a: ");
+    scanf("%f",&a);
+    printf("%f",a);
+
+    return 0;
+}
