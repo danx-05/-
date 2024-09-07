cfcf4ab2f9a37b094dbee635abfe8dd1ecd8d291
danx-05 <danekdenek203@gmail.com>
2024-09-07 10:54:01 +0500

    Update task4.c

--- labs/lab1/task4.c
+++ labs/lab1/task4.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-int main()
-{
-    
-    float a;
-    float b;
-    float c;
-    
-    
+int main(){
+    float a,b,c,x;
     printf("Input a: ");
-    scanf("%f",&a);
-    printf("%f",a);
-
+    scanf("%f", &a);
+    printf("Input b: ");
+    scanf("%f", &b);
+    printf("Input c: ");
+    scanf("%f", &c);
+    
+    if (a == 0){
+        if (b == c) {printf("equation has infinitely many solutions\n");}
+        else{ printf("equation has not solution");}
+    } else{
+        x = (c - b) / a;
+        printf("x=%.1f\n",x);
+    }
     return 0;
 }
