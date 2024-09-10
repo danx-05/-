b31200ac196fcc5d1f6f6c3cfeda9d6094eb12cf
danx-05 <danekdenek203@gmail.com>
2024-09-10 10:22:55 +0500

    Update task2.c

--- labs/lab2/task2.c
+++ labs/lab2/task2.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
+int main()
+{
+    srand(time(NULL));
+    int size = rand() % 20;
+    int arr[size];
+    
+    for (int i = 0; i < size; i++){
+        arr[i] = rand()%100 * pow(-1,rand()%2);
+        printf("%d ", arr[i]);
+        
+    }
+    printf("\n");
+    for (int i = size - 1; i >= 0; i--)
+    printf("%d ", arr[i]);
+    return 0;
+}
