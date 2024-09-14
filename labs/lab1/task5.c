cacc9c5877ec8c2a880585d69acc34b54e7da67d
danx-05 <danekdenek203@gmail.com>
2024-09-14 13:32:17 +0500

    Update task5.c

--- labs/lab1/task5.c
+++ labs/lab1/task5.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Ошибка при открытии файла");
+        return 1;
+    }
+    char line[256];
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line);
+    }
+    fclose(file);
+    return 0;
+}
