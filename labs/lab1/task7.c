ce4c42745997ee14eaefbe9079803638c81f7ade
danx-05 <danekdenek203@gmail.com>
2024-09-16 11:34:51 +0500

    Update task7.c

--- labs/lab1/task7.c
+++ labs/lab1/task7.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main() {
+    FILE *file;
+    char str[] = "\nНовая строчка";
+    file = fopen("a2", "a");
+    if (file == NULL) {
+        perror("Ошибка при открытии файла");
+        return 1;
+    }
+    fprintf(file, "%s", str);
+    fclose(file);
+    
+    return 0;
+}
