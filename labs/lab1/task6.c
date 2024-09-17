6fb1bd1acb991b9ddf8e5c628eeb57aabec8f04c
danx-05 <danekdenek203@gmail.com>
2024-09-17 09:56:27 +0500

    Update task6.c

--- labs/lab1/task6.c
+++ labs/lab1/task6.c
@@ -1,1 +1,23 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+    FILE *inputfile = fopen(argv[1], "r");
+    if (inputfile == NULL) {
+        perror("Ошибка при открытии файла");
+        return 1;
+    }
+    FILE *outfile = fopen(argv[2], "w");
+    if (outfile == NULL) {
+        perror("Ошибка при открытии файла");
+        return 1;
+    }
+    char line[256];
+    int c = 1;
+    while (fgets(line, sizeof(line), inputfile) != NULL && c < 4) {
+        fputs(line, outfile);
+        c++;
+    }
+    fclose(inputfile);
+    fclose(outfile);
+    return 0;
+}
