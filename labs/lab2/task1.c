e9b4a0131514c3611eace1ed08f17a5bd1015e23
danx-05 <danekdenek203@gmail.com>
2024-09-22 20:22:08 +0500

    Update task1.c

--- labs/lab2/task1.c
+++ labs/lab2/task1.c
@@ -7,7 +7,6 @@ int main()
     srand(time(NULL));
     int size = rand() % 20;
     int arr[size];
-    
     for (int i = 0; i < size; i++){
         arr[i] = rand()%100;
         printf("%d ", arr[i]);
