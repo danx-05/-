f82e7b112249df1bad0984695f59aff3e5d50008
danx-05 <danekdenek203@gmail.com>
2024-09-22 20:24:00 +0500

    Update task2.c

--- labs/lab2/task2.c
+++ labs/lab2/task2.c
@@ -2,20 +2,26 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-
 int main()
 {
     srand(time(NULL));
     int size = rand() % 20;
-    int arr[size];
-    
-    for (int i = 0; i < size; i++){
-        arr[i] = rand()%100 * pow(-1,rand()%2);
+    int arr[1 + size];
+    printf("Array: ");
+    for (int i = 0; i < size + 1; i++){
+        arr[i] = rand()%1000 * pow(-1,rand()%2);
         printf("%d ", arr[i]);
-        
     }
     printf("\n");
-    for (int i = size - 1; i >= 0; i--)
-    printf("%d ", arr[i]);
+    FILE *file;
+    file = fopen("task2","a");
+    if (file == NULL) {
+        printf("Error\n");
+	return 1;
+    }
+    for (int i = size; i >= 0; i--)
+    fprintf(file,"%d ", arr[i]);
+    fprintf(file,"\n");
+    fclose(file);
     return 0;
 }
