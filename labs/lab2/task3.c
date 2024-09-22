6b9788b313a5ab9da4f03152e50385e0e8f50be1
danx-05 <danekdenek203@gmail.com>
2024-09-22 20:25:03 +0500

    Update task3.c

--- labs/lab2/task3.c
+++ labs/lab2/task3.c
@@ -3,20 +3,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    int size;
-    printf("Input size: ");
-    scanf("%d",&size);
+    if (argc < 2){return 1;}
+    int size = atoi(argv[1]);
+    if (size < 1){
+	printf("Error");
+	return 1;
+    }
     float arr[size];
-    
     for (int i = 0; i < size; i++){
         arr[i] = (float)rand()/RAND_MAX;
         printf("%f ", arr[i]);
-        
     }
     printf("\n");
-    
     return 0;
 }
