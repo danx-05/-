5d6ecc8f9f7af0e32882fbecb270c8c8137d5de3
danx-05 <danekdenek203@gmail.com>
2024-09-24 09:48:36 +0500

    Update task8.c

--- labs/lab1/task8.c
+++ labs/lab1/task8.c
@@ -8,23 +8,19 @@ int main() {
     printf("Input new str: ");
     fgets(new_str,100,stdin);
     new_str[strcspn(new_str,"\n")] = '\0';
-
     input_file = fopen("t8","r");
     if (input_file == NULL){
         return 1;
     }
     temp_file = fopen("temp","w");
-    
     fprintf(temp_file, "%s\n", new_str);
     while (fgets(line,100,input_file) != NULL){
         fputs(line, temp_file);
     }
-    
     fclose(input_file);
     fclose(temp_file);
     input_file = fopen("t8","w");
     temp_file = fopen("temp", "r");
-    
     while (fgets(line, 100, temp_file) != NULL){
         fputs(line,input_file);
     }
