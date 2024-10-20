cad0f2b1cb135a35b80600be950a7ffaee9976bc
danx-05 <danekdenek203@gmail.com>
2024-10-20 16:27:23 +0500

    Update task9.c

--- labs/lab2/task9.c
+++ labs/lab2/task9.c
@@ -1,1 +1,83 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+const char* input;
+char l;
+void expr();
+void exprRest();
+void term();
+void termRest();
+void factor();
+void match(char expected);
+void error();
+void getNextToken() {
+    l = *input++;
+}
+void match(char expected) {
+    if (l == expected) {
+        getNextToken();
+    } else {
+        error();
+    }
+}
+void error() {
+    printf("Error\n");
+    exit(1);
+}
 
+int main() {
+    printf("Введите инфиксное выражение: ");
+    char buffer[100];
+    fgets(buffer, sizeof(buffer), stdin);
+    input = buffer;
+    getNextToken();
+    expr();
+    printf("\n");
+    return 0;
+}
+void expr() {
+    term();
+    exprRest();
+}
+void exprRest() {
+    if (l == '+') {
+        match('+');
+        term();
+        exprRest();
+        printf("+");
+    } else if (l == '-') {
+        match('-');
+        term();
+        exprRest();
+        printf("-");
+    }
+}
+void term() {
+    factor();
+    termRest();
+}
+void termRest() {
+    if (l == '*') {
+        match('*');
+        factor();
+        termRest();
+        printf("*");
+    } else if (l == '/') {
+        match('/');
+        factor();
+        termRest();
+        printf("/");
+    }
+}
+void factor() {
+    if (isdigit(l)) {
+        printf("%c",l);
+        match(l);
+    } else if (l == '(') {
+        match('(');
+        expr();
+        match(')');
+    } else {
+        error();
+    }
+}
