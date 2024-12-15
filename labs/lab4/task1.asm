19a0f5b55f190e0cbe5da0a001d782a3a887d182
danx-05 <danekdenek203@gmail.com>
2024-12-15 15:18:06 +0500

    Update task1.asm

--- labs/lab4/task1.asm
+++ labs/lab4/task1.asm
@@ -1,8 +1,6 @@
-.ORIG x3000
-
-    LEA R0, HELLO_STR    ; загрузка адреса строки в R0
+.ORIG x3000               ; начало программы
+    LEA R0, HELLO_STR     ; загрузка адреса строки в R0
     PUTS                  ; вывод строки
     HALT                  ; остановка программы
-HELLO_STR .STRINGZ "Hello, World!"
-
+HELLO_STR .STRINGZ "Hello, World!" ; определение строки
 .END
